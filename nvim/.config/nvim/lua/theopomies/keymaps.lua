d555ec1aa7701817dddb335b18fed5c8d8e0d7ce
Théo Pomies <theo.pomies@outlook.com>
2022-03-22 17:58:15 +0100

    feat: add: qflist

--- nvim/.config/nvim/lua/theopomies/keymaps.lua
+++ nvim/.config/nvim/lua/theopomies/keymaps.lua
@@ -85,7 +85,37 @@ keymap("n", "H", "`", opts)
 keymap("n", "<leader>h", "'", opts)
 
 -- QuickFix List
--- TODO When needed
+vim.cmd([[
+nnoremap <C-l> :cnext<CR>zz
+nnoremap <C-k> :cprev<CR>zz
+nnoremap <leader>l :lnext<CR>zz
+nnoremap <leader>k :lprev<CR>zz
+nnoremap <C-q> :call ToggleQFList(1)<CR>
+nnoremap <leader>q :call ToggleQFList(0)<CR>
+
+let g:the_primeagen_qf_l = 0
+let g:the_primeagen_qf_g = 0
+
+fun! ToggleQFList(global)
+    if a:global
+        if g:the_primeagen_qf_g == 1
+            let g:the_primeagen_qf_g = 0
+            cclose
+        else
+            let g:the_primeagen_qf_g = 1
+            copen
+        end
+    else
+        if g:the_primeagen_qf_l == 1
+            let g:the_primeagen_qf_l = 0
+            lclose
+        else
+            let g:the_primeagen_qf_l = 1
+            lopen
+        end
+    endif
+endfun
+]])
 
 -- Shift multiple lines
 keymap("n", "<TAB>", ":><CR>", opts)
